be4b21b9ebe1735cde5e8b15f744960f7ede9b10
Vitalii skus <vskus>
2023-02-15 10:02:43 +0200

    Configure less

--- src/Components/Tabs/View.elm
+++ src/Components/Tabs/View.elm
@@ -1,11 +1,12 @@
 module Components.Tabs.View exposing (..)
 
 import Html exposing (Html, text, div)
+import Html.Attributes exposing (class)
 
 import Messages exposing (Msg)
 
 
 viewTabs : List String -> Html Msg
 viewTabs tabs =
-    div []
-        (List.map (\l -> div [] [ text l ]) tabs)
+    div [class "tab"]
+        (List.map (\l -> div [class "tab-item"] [ text l ]) tabs)
